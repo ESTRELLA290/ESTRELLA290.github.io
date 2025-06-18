f56223b25722ed1d68393fd84b2dbda6600b54ac
ESTRELLA290 <estrellamtz290@gmail.com>
2025-06-17 23:32:58 -0600

    n

--- despedida-prepa/js/script.js
+++
@@ -1,7 +0,0 @@
-function mostrarSeccion(id) {
-  const secciones = document.querySelectorAll('.seccion');
-  secciones.forEach(sec => sec.style.display = 'none');
-
-  const activa = document.getElementById(id);
-  if (activa) activa.style.display = 'block';
-}
