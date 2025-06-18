937482d7bd06718fa29135b56ef2da0f44738461
ESTRELLA290 <estrellamtz290@gmail.com>
2025-06-17 23:33:43 -0600

    bien

---
+++ despedida-prepa/js/script.js
@@ -0,0 +1,25 @@
+function mostrarSeccion(id) {
+  const secciones = document.querySelectorAll('.seccion');
+  secciones.forEach(sec => sec.classList.add('oculto'));
+
+  const activa = document.getElementById(id);
+  if (activa) activa.classList.remove('oculto');
+}
+
+window.onload = function () {
+  mostrarSeccion('galeria');
+
+  const imagenes = document.querySelectorAll('.seccion img');
+  imagenes.forEach(img => {
+    img.addEventListener('click', function () {
+      const modal = document.getElementById('modal');
+      const modalImg = document.getElementById('modal-img');
+      modal.style.display = 'flex';
+      modalImg.src = this.src;
+    });
+  });
+};
+
+function cerrarModal() {
+  document.getElementById('modal').style.display = 'none';
+}
